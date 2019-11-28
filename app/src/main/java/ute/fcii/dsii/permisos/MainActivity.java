37977f423fa3d743f1d8e854742fb5c4c010ba21
daoc <diego.ordonez@gmail.com>
2019-11-28 10:27:52 -0500

    Update

---
+++ app/src/main/java/ute/fcii/dsii/permisos/MainActivity.java
@@ -0,0 +1,63 @@
+package ute.fcii.dsii.permisos;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pidePermiso(View v) {
+        int permissionCheck = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED) {
+            trabajaConArchivo();
+        } else if(permissionCheck == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }
+    }
+
+    private void trabajaConArchivo() {
+        try {
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "mitexto.txt");
+            file.createNewFile();
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write("Este es mi mensaje".getBytes());
+            fos.close();
+            TextView tv = (TextView) findViewById(R.id.tv);
+            tv.setText("Funcionó");
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permiso concedido!!!", Toast.LENGTH_LONG).show();
+                trabajaConArchivo();
+            } else {
+                Toast.makeText(this, "Permiso negado XXX", Toast.LENGTH_LONG).show();
+                trabajaConArchivo();//Esto debe fallar y presentar errores !!!
+            }
+        } else {
+            Toast.makeText(this, "Pedido desconocido", Toast.LENGTH_LONG).show();
+        }
+    }
+}
